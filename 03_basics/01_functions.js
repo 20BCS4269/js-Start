0160d26a54db90dc3e56e246b6d3242839416eb9
20BCS4269 <110817534+20BCS4269@users.noreply.github.com>
2023-12-29 19:02:09 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,35 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("abhishek"))
-console.log(loginUserMessage("abhishek"))
+// console.log(loginUserMessage("abhishek"))
+
+
+function claculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(claculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "abhishek",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 300, 600, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 600, 400]));
